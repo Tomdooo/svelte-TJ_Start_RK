ba2467286d1a8649b947ac65467155191bc3fba9
Tomdooo_ <tomdo_@fpproduction.eu>
2022-12-22 12:07:05 +0100

    some basic tutorial :)

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    let body = {
+        username: "hej",
+        password: ""
+    }
+
+    async function submitForm() {
+
+    }
+</script>
+
+
+<form on:click|preventDefault={submitForm}>
+    <label>Uživatelské jméno: <input type="text" bind:value={body.username} /></label>
+    <label>Heslo: <input type="password" value="" /></label>
+    <button type="submit">Login</button>
+</form>
+
+
+<style>
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
