b44748f70572c96b15a5fe04b20c89223f80ed54
Václav Buřil <vasekburil@seznam.cz>
2022-12-28 13:17:12 +0100

    Add Popup

---
+++ src/components/PopupDeleteUser.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let name = "aaaaaaaaaaaa";
+
+</script>
+
+<p>{name}</p>
--- src/components/TableUsers.svelte
+++ src/components/TableUsers.svelte
@@ -1,7 +1,12 @@
 <script>
+    let deleteId = {
+        id:1
+
+    };
     //TODO doplnit z DB
     let members = [
         {
+            id:1,
             role: "ADMIN",
             firstName: "Václav",
             lastName: "Buřil",
@@ -11,6 +16,16 @@
             },
         },
         {
+            id:2,
+            role: "USER",
+            firstName: "Tomáš",
+            lastName: "Němeček",
+            username: "tomas",
+            team: {
+                id: 2
+            },
+        }, {
+            id:3,
             role: "USER",
             firstName: "Tomáš",
             lastName: "Němeček",
@@ -19,11 +34,33 @@
                 id: 2
             },
         }
-    ]
+    ];
+
+    const setName = (id) => {deleteId.id = id; console.log(deleteId.id)} ;
+
+
+    //name =  document.getElementById("userName-td").value();
+
+import { writable } from 'svelte/store';
+ import Modal, {bind} from 'svelte-simple-modal';
+   import Popup from './PopupDeleteUser.svelte';
+   const modal = writable(null);
+     const showModal = (name) => modal.set(bind (Popup, {name: name}));
+
+   // import Modal from './Modal.svelte';
+    //
+     let showModalB = false;
 </script>
+{#if showModalB}
+    <Modal on:close="{() => showModalB = false}">
+        {showModal.call()}
+    </Modal>
+{/if}
+
 
 <div id="table-list">
     <table id="users_table">
+
         <tr>
             <th>Jméno</th>
             <th>Příjmení</th>
@@ -55,7 +92,16 @@
                     <!--  TODO  <a id="update_table" href="/{{this.id}}/update">Upravit</a>-->
                 </td>
                 <td id="delete-table">
-                    Odstranit
+
+
+                        <a on:click={() => {setName(member.id) ;showModalB = true}}>Odstranit</a>
+<!--                        {#if member.id === deleteId.id}-->
+<!--                            <Modal show={$modal}>-->
+<!--                        <a on:click={() => {showModal(member.username)}}>Odstranit</a>-->
+<!--                            </Modal>-->
+<!--                        {/if}-->
+
+
                     <!--  TODO  <a id="delete-table" href="/{{this.id}}/delete">Odstranit</a>-->
                 </td>
             </tr>
